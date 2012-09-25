0114ba26faf6e2b53b64ba63afe7d9014bc89757
Hojung Youn <amoc.yn@gmail.com>
2012-09-25 14:01:54 +0900

    Fix MoreHelpers plugin to be compatible with Mojolicious 3.43

--- MoreHelpers/lib/Mojolicious/Plugin/MoreHelpers.pm
+++ MoreHelpers/lib/Mojolicious/Plugin/MoreHelpers.pm
@@ -1,21 +1,45 @@
 package Mojolicious::Plugin::MoreHelpers;
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojolicious::Plugin::DefaultHelpers;
+use Mojolicious 3.43;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub register {
     my ($self, $app) = @_;
     
-    $app->helper(content_after => \&_content_helper);
+    $app->helper(content_after => \&_content_after);
 }
 
-sub _content_helper {
-    my ($self, $name) = (shift, shift);
-    local *_content = \&Mojolicious::Plugin::DefaultHelpers::_content;
-    _content($self, $name, @_, _content($self, $name));
+sub _content {
+    my ($self, $name, $content) = @_;
+    $name ||= 'content';
+
+    # Set (first come)
+    my $c = $self->stash->{'mojo.content'} ||= {};
+    $c->{$name} ||= ref $content eq 'CODE' ? $content->() : $content
+        if defined $content;
+
+    # Get
+    return Mojo::ByteStream->new($c->{$name} // '');
 }
 
+# sub _content_for {
+#     my ($self, $name, $content) = @_;
+#     return _content($self, $name) unless defined $content;
+#     my $c = $self->stash->{'mojo.content'} ||= {};
+#     return $c->{$name} .= ref $content eq 'CODE' ? $content->() : $content;
+# }
+
+sub _content_after {
+    my ($self, $name, $content) = @_;
+    return _content($self, $name) unless defined $content;
+    my $c = $self->stash->{'mojo.content'} ||= {};
+    my $t = (ref $content eq 'CODE' ? $content->() : $content);
+    $c->{$name} = '' unless defined $c->{$name};
+    $c->{$name} = $t . $c->{$name};
+    return $c->{$name};
+}
 
 1;
 __END__
